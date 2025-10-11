8365f897ec2a87d04133d1b104c67dd9c4629212
zzp <13556632276@163.com>
2025-10-11 08:51:09 +0800

    创建IOC容器并实现依赖注入

--- 无人值守地磅称重系统课程/App.xaml.cs
+++ 无人值守地磅称重系统课程/App.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using 无人值守地磅称重系统课程.Configure;
+using 无人值守地磅称重系统课程.Views;
 
 namespace 无人值守地磅称重系统课程
 {
@@ -13,5 +16,20 @@ namespace 无人值守地磅称重系统课程
     /// </summary>
     public partial class App : Application
     {
+        public new static App Current => (App)Application.Current;
+
+        public IServiceProvider Services { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Services.GetService<ShellView>()?.Show();
+        }
+
+        public App()
+        {
+            Services = ConfigureService.Load();
+        }
     }
 }
---
+++ 无人值守地磅称重系统课程/Configure/ConfigureService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 无人值守地磅称重系统课程.ViewModels;
+using 无人值守地磅称重系统课程.Views;
+
+namespace 无人值守地磅称重系统课程.Configure
+{
+    internal class ConfigureService
+    {
+        public static IServiceProvider Load()
+        {
+            var services = new ServiceCollection();
+
+            // 注册UI层
+            services.AddSingleton<ShellView>();
+            services.AddSingleton<ShellViewModel>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
