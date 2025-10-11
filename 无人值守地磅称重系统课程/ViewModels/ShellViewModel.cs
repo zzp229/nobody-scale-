9221b4c5a55c8756ca83940215d2bcb8028ef3e4
zzp <13556632276@163.com>
2025-10-11 08:40:51 +0800

    添加项目文件。

---
+++ 无人值守地磅称重系统课程/ViewModels/ShellViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 无人值守地磅称重系统课程.ViewModels
+{
+    internal class ShellViewModel
+    {
+    }
+}
