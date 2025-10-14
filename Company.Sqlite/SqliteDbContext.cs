9794451b1eb2edeb59621a38568875da0694c5be
zzp <13556632276@163.com>
2025-10-14 21:12:07 +0800

    Sqlite数据库组件安装和连接字符串配置

---
+++ Company.Sqlite/SqliteDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Sqlite
+{
+    internal class SqliteDbContext : DbContext
+    {
+        public SqliteDbContext() : base("SqliteDbContext")
+        {
+            
+        }
+    }
+}
