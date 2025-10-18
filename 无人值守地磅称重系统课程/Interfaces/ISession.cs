58e715f954b457404503f7bb1950d3cf35e7e92e
zzp <13556632276@163.com>
2025-10-18 15:20:02 +0800

    测试

---
+++ 无人值守地磅称重系统课程/Interfaces/ISession.cs
@@ -0,0 +1,15 @@
+﻿using Company.Sqlite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 无人值守地磅称重系统课程.Interfaces
+{
+    public interface ISession
+    {
+        string Title { get; set; }
+        User CurrentUser { get; set; }
+    }
+}
---
+++ 无人值守地磅称重系统课程/Services/Session.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Company.Sqlite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 无人值守地磅称重系统课程.Interfaces;
+
+namespace 无人值守地磅称重系统课程.Services
+{
+    public class Session : ObservableObject, ISession
+    {
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { SetProperty(ref title, value); }
+        }
+          
+        private User user = new User();
+
+        public User CurrentUser
+        {
+            get { return user; }
+            set { SetProperty(ref user, value); }
+        }
+
+
+    }
+}
